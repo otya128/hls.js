otya128/hls.js

commit 60da6e192981591456cfb5fb8bf3768c59a31ff2

Fix AV desync regression in v1.4.0 when mp4 audio track timestamps start before video track timestamps (#5471)
Fixes #5452

src/remux/passthrough-remuxer.ts
@@ -167,20 +167,25 @@ class PassThroughRemuxer implements Remuxer {
       this.emitInitSegment = false;
     }
 
+    const duration = getDuration(data, initData);
     const startDTS = getStartDTS(initData, data);
     const decodeTime = startDTS === null ? timeOffset : startDTS;
     if (
-      isInvalidInitPts(initPTS, decodeTime, timeOffset) ||
+      isInvalidInitPts(initPTS, decodeTime, timeOffset, duration) ||
       (initSegment.timescale !== initPTS.timescale && accurateTimeOffset)
     ) {
       initSegment.initPTS = decodeTime - timeOffset;
+      if (initPTS && initPTS.timescale === 1) {
+        logger.warn(
+          `Adjusting initPTS by ${initSegment.initPTS - initPTS.baseTime}`
+        );
+      }
       this.initPTS = initPTS = {
         baseTime: initSegment.initPTS,
         timescale: 1,
       };
     }
 
-    const duration = getDuration(data, initData);
     const startTime = audioTrack
       ? decodeTime - initPTS.baseTime / initPTS.timescale
       : (lastEndTime as number);
@@ -244,14 +249,16 @@ class PassThroughRemuxer implements Remuxer {
 function isInvalidInitPts(
   initPTS: RationalTimestamp | null,
   startDTS: number,
-  timeOffset: number
+  timeOffset: number,
+  duration: number
 ): initPTS is null {
   if (initPTS === null) {
     return true;
   }
-  // InitPTS is invalid when it would cause start time to be negative, or distance from time offset to be more than 1 second
+  // InitPTS is invalid when distance from program would be more than segment duration or a minimum of one second
+  const minDuration = Math.max(duration, 1);
   const startTime = startDTS - initPTS.baseTime / initPTS.timescale;
-  return startTime < 0 && Math.abs(startTime - timeOffset) > 1;
+  return Math.abs(startTime - timeOffset) > minDuration;
 }
 
 function getParsedTrackCodec(

commit 583cf45581381df510900fd46c1d1ae09aa7b3ef

Don't adjust initPTS for fmp4 non-first moof  in progressive mode
Fix regression introduced in #5471.

src/demux/transmuxer-interface.ts
@@ -224,7 +224,8 @@ export default class TransmuxerInterface {
       accurateTimeOffset,
       trackSwitch,
       timeOffset,
-      initSegmentChange
+      initSegmentChange,
+      progressive
     );
     if (!contiguous || discontinuity || initSegmentChange) {
       logger.log(`[transmuxer-interface, ${frag.type}]: Starting new transmux session for sn: ${chunkMeta.sn} p: ${chunkMeta.part} level: ${chunkMeta.level} id: ${chunkMeta.id}
@@ -233,7 +234,8 @@ export default class TransmuxerInterface {
         contiguous: ${contiguous}
         accurateTimeOffset: ${accurateTimeOffset}
         timeOffset: ${timeOffset}
-        initSegmentChange: ${initSegmentChange}`);
+        initSegmentChange: ${initSegmentChange}
+        progressive: ${progressive}`);
       const config = new TransmuxConfig(
         audioCodec,
         videoCodec,

src/demux/transmuxer.ts
@@ -103,6 +103,7 @@ export default class Transmuxer {
       accurateTimeOffset,
       timeOffset,
       initSegmentChange,
+      progressive,
     } = state || currentTransmuxState;
     const {
       audioCodec,
@@ -192,7 +193,8 @@ export default class Transmuxer {
       keyData,
       timeOffset,
       accurateTimeOffset,
-      chunkMeta
+      chunkMeta,
+      progressive
     );
     const currentState = this.currentTransmuxState;
 
@@ -262,7 +264,8 @@ export default class Transmuxer {
     chunkMeta: ChunkMetadata
   ) {
     const { audioTrack, videoTrack, id3Track, textTrack } = demuxResult;
-    const { accurateTimeOffset, timeOffset } = this.currentTransmuxState;
+    const { accurateTimeOffset, timeOffset, progressive } =
+      this.currentTransmuxState;
     logger.log(
       `[transmuxer.ts]: Flushed fragment ${chunkMeta.sn}${
         chunkMeta.part > -1 ? ' p: ' + chunkMeta.part : ''
@@ -276,7 +279,8 @@ export default class Transmuxer {
       timeOffset,
       accurateTimeOffset,
       true,
-      this.id
+      this.id,
+      progressive
     );
     transmuxResults.push({
       remuxResult,
@@ -345,7 +349,8 @@ export default class Transmuxer {
     keyData: KeyData | null,
     timeOffset: number,
     accurateTimeOffset: boolean,
-    chunkMeta: ChunkMetadata
+    chunkMeta: ChunkMetadata,
+    progressive: boolean
   ): TransmuxerResult | Promise<TransmuxerResult> {
     let result: TransmuxerResult | Promise<TransmuxerResult>;
     if (keyData && keyData.method === 'SAMPLE-AES') {
@@ -354,14 +359,16 @@ export default class Transmuxer {
         keyData,
         timeOffset,
         accurateTimeOffset,
-        chunkMeta
+        chunkMeta,
+        progressive
       );
     } else {
       result = this.transmuxUnencrypted(
         data,
         timeOffset,
         accurateTimeOffset,
-        chunkMeta
+        chunkMeta,
+        progressive
       );
     }
     return result;
@@ -371,7 +378,8 @@ export default class Transmuxer {
     data: Uint8Array,
     timeOffset: number,
     accurateTimeOffset: boolean,
-    chunkMeta: ChunkMetadata
+    chunkMeta: ChunkMetadata,
+    progressive: boolean
   ): TransmuxerResult {
     const { audioTrack, videoTrack, id3Track, textTrack } = (
       this.demuxer as Demuxer
@@ -384,7 +392,8 @@ export default class Transmuxer {
       timeOffset,
       accurateTimeOffset,
       false,
-      this.id
+      this.id,
+      progressive
     );
     return {
       remuxResult,
@@ -397,7 +406,8 @@ export default class Transmuxer {
     decryptData: KeyData,
     timeOffset: number,
     accurateTimeOffset: boolean,
-    chunkMeta: ChunkMetadata
+    chunkMeta: ChunkMetadata,
+    progressive: boolean
   ): Promise<TransmuxerResult> {
     return (this.demuxer as Demuxer)
       .demuxSampleAes(data, decryptData, timeOffset)
@@ -410,7 +420,8 @@ export default class Transmuxer {
           timeOffset,
           accurateTimeOffset,
           false,
-          this.id
+          this.id,
+          progressive
         );
         return {
           remuxResult,
@@ -516,6 +527,7 @@ export class TransmuxState {
   public trackSwitch: boolean;
   public timeOffset: number;
   public initSegmentChange: boolean;
+  public progressive: boolean;
 
   constructor(
     discontinuity: boolean,
@@ -523,7 +535,8 @@ export class TransmuxState {
     accurateTimeOffset: boolean,
     trackSwitch: boolean,
     timeOffset: number,
-    initSegmentChange: boolean
+    initSegmentChange: boolean,
+    progressive: boolean
   ) {
     this.discontinuity = discontinuity;
     this.contiguous = contiguous;
@@ -531,5 +544,6 @@ export class TransmuxState {
     this.trackSwitch = trackSwitch;
     this.timeOffset = timeOffset;
     this.initSegmentChange = initSegmentChange;
+    this.progressive = progressive;
   }
 }

src/remux/passthrough-remuxer.ts
@@ -124,7 +124,10 @@ class PassThroughRemuxer implements Remuxer {
     id3Track: DemuxedMetadataTrack,
     textTrack: DemuxedUserdataTrack,
     timeOffset: number,
-    accurateTimeOffset: boolean
+    accurateTimeOffset: boolean,
+    _flush: boolean,
+    _playlistType: never,
+    progressive: boolean
   ): RemuxerResult {
     let { initPTS, lastEndTime } = this;
     const result: RemuxerResult = {
@@ -172,7 +175,13 @@ class PassThroughRemuxer implements Remuxer {
     const startDTS = getStartDTS(initData, data);
     const decodeTime = startDTS === null ? timeOffset : startDTS;
     if (
-      isInvalidInitPts(initPTS, decodeTime, timeOffset, duration) ||
+      isInvalidInitPts(
+        initPTS,
+        decodeTime,
+        timeOffset,
+        duration,
+        progressive
+      ) ||
       (initSegment.timescale !== initPTS.timescale && accurateTimeOffset)
     ) {
       initSegment.initPTS = decodeTime - timeOffset;
@@ -251,10 +260,14 @@ function isInvalidInitPts(
   initPTS: RationalTimestamp | null,
   startDTS: number,
   timeOffset: number,
-  duration: number
+  duration: number,
+  progressive: boolean
 ): initPTS is null {
   if (initPTS === null) {
     return true;
+  }
+  if (progressive) {
+    return false;
   }
   // InitPTS is invalid when distance from program would be more than segment duration or a minimum of one second
   const minDuration = Math.max(duration, 1);

src/types/remuxer.ts
@@ -21,7 +21,8 @@ export interface Remuxer {
     timeOffset: number,
     accurateTimeOffset: boolean,
     flush: boolean,
-    playlistType: PlaylistLevelType
+    playlistType: PlaylistLevelType,
+    progressive: boolean
   ): RemuxerResult;
   resetInitSegment(
     initSegment: Uint8Array | undefined,

tests/unit/demuxer/transmuxer.ts
@@ -131,7 +131,7 @@ describe('TransmuxerInterface tests', function () {
     const duration = 0;
     const accurateTimeOffset = true;
     let chunkMeta = new ChunkMetadata(currentFrag.level, currentFrag.sn + 1, 0);
-    let state = new TransmuxState(false, true, true, false, 0, false);
+    let state = new TransmuxState(false, true, true, false, 0, false, false);
     transmuxerInterface.push(
       data,
       initSegmentData,
@@ -164,7 +164,7 @@ describe('TransmuxerInterface tests', function () {
     newFrag.start = 1000;
     newFrag.startPTS = 1000;
     chunkMeta = new ChunkMetadata(newFrag.level, newFrag.sn, 0);
-    state = new TransmuxState(false, true, true, false, 1000, false);
+    state = new TransmuxState(false, true, true, false, 1000, false, false);
     transmuxerInterface.push(
       data,
       initSegmentData,
@@ -245,7 +245,15 @@ describe('TransmuxerInterface tests', function () {
     );
 
     const tConfig = new TransmuxConfig('', '', initSegmentData, 0);
-    const state = new TransmuxState(true, false, true, true, 1000, false);
+    const state = new TransmuxState(
+      true,
+      false,
+      true,
+      true,
+      1000,
+      false,
+      false
+    );
     expect(configureStub).to.have.been.calledOnce;
     expect(configureStub).to.have.been.calledWith(tConfig);
 